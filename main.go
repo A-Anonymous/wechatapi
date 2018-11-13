eb1deb8db632f40304087285355fc5a4107605f5
“A-Anonymous” <1245800723@qq.com>
2018-11-13 16:59:52 +0800

    xml name

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
 	"github.com/astaxie/beego"
+	"os"
 	_ "wechatapi/routers"
 )
 
 func main() {
 
+	//test()
+
 	beego.Info(beego.BConfig.AppName, "V0.1")
 
 	beego.Run()
+}
+
+func test (){
+	type Address struct {
+		City, State string
+	}
+	type Person struct {
+		XMLName   xml.Name `xml:"xml"`
+		Id        int      `xml:"id,attr"`
+		FirstName string   `xml:"name>first"`
+		LastName  string   `xml:"name>last"`
+		Age       int      `xml:"age"`
+		Height    float32  `xml:"height,omitempty"`
+		Married   bool
+		Address
+		Comment string `xml:",comment"`
+	}
+
+	v := &Person{Id: 13, FirstName: "John", LastName: "Doe", Age: 42}
+	v.Comment = " Need more details. "
+	v.Address = Address{"Hanga Roa", "Easter Island"}
+
+	output, err := xml.MarshalIndent(v, "   ", "    ")
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+	}
+
+	os.Stdout.Write(output)
 }
